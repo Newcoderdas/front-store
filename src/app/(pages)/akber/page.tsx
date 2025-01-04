de0929193649429155136531bc8541fbfa78e7b2
Syed Rizwan Hussain <106629183+Newcoderdas@users.noreply.github.com>
2025-01-05 02:07:53 +0500

    hy

---
+++ src/app/(pages)/akber/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Akber Page</div>
+  )
+}
+
+export default page
---
+++ src/app/components/Bar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Bar = () => {
+  return (
+    <div className="flex  bg-blue-700 p-10 gap-3 underline  items-center justify-center max-w-[500px] rounded-lg  w-full mx-auto  mt-44 text-white font-bold ">
+
+         
+              
+    <Link href="/akber" className='hover:text-gray-300'> Akber ka Page</Link>
+    <Link href="/raghib" className='hover:text-gray-300'>raghib ka Page</Link>
+    
+     </div>
+  )
+}
+
+export default Bar
