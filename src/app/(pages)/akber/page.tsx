5082cd5a45664060033bfd9bc23dbb807ad8071b
Rizwan <syed.51037@iqra.edu.pk>
2025-01-06 17:31:50 +0500

    my second commit

--- src/app/(pages)/akber/page.tsx
+++ src/app/(pages)/akber/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Akber Page</div>
+    <>
+    <div>Akber Page
+      
+      </div>
+      <div className='bg-slate-300 '>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="bg-white p-4  rounded-lg shadow-md w-[35rem] h-[20rem]">
+          
+            <div className="bg-black p-10 rounded-full shadow-md w-4 ">
+             </div>
+             <div className=' text-center mb-20'>
+              <h1>name </h1>  
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
