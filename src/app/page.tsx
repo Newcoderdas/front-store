cb79f2edeedc2be785522ad866adf0ce4035535a
Rizwan <syed.51037@iqra.edu.pk>
2025-01-07 02:34:18 +0500

    second time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Bar from "./components/Bar";
+import Bar from "./components/Navbar";
 
 export default function Home() {
   return (
