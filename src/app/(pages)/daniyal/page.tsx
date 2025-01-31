3d3a3798de76a446cf90b0b7960c6bbde204369a
Syed Rizwan Hussain <106629183+Newcoderdas@users.noreply.github.com>
2025-01-31 14:46:56 +0500

    mm

--- src/app/(pages)/daniyal/page.tsx
+++ src/app/(pages)/daniyal/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Daniyal Page</div>
+      <div className='flex flex-row text-xl   items-center font-bold text-white max-w-[500px]  mx-auto mt-44 p-36  rounded-3xl shadow-lg bg-black'>
+          <div className='flex gap-5'>
+              <Image src="/download.jpg" height={32} width={32} alt='image' className='rounded-full bg-cover bg-center'/><h1>| Daniyal Page</h1></div>
+          </div>
   )
 }
 
